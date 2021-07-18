688c422bda5f2f6ad260aeb9aca185f8a2d82280
Jowel Tisso <52632590+JowelTisso@users.noreply.github.com>
2021-07-18 06:11:15 +0000

    Update: Changed the quiz theme

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 readlineSync = require('readline-sync');
 var chalk = require('chalk');
 
-console.log(chalk.cyanBright.bold("Welcome to Jowel's quiz on FRIENDS"))
-console.log(' ')
+console.log(chalk.cyanBright.bold("Welcome to Game Quiz!"))
 
 var score = 0;
 var userName = readlineSync.question(chalk.whiteBright.bold('Whats your name? '));
 
-console.log("Welcome "+ userName+ "! Do you know Jowel? Let's find out!")
+console.log("Welcome "+ userName+ "! Are you a fan of Mobile Legend? Let's find out!")
 
 console.log(chalk.cyanBright.bold("Note: Answer with typing only options, eg: a or b"))
 
@@ -23,35 +22,35 @@ var userAnswer = readlineSync.question(question)
 }
 
 var questions = [{
-  question: `Where does Jowel live? 
-  a : Guwahati
-  b : Jorhat
-  c : Silchar
+  question: `What type of game is Mobile Legend? 
+  a : MOBA
+  b : RPG
+  c : RACING
 
   `,
   answer:"a"
 }, {
-  question: `What is Jowel's favourite fastfood? 
-  a : Roll
-  b : Momo
-  c : Dosa
+  question: `Which company does Mobile Legend belong to? 
+  a : Tencent
+  b : Montoon
+  c : Valve
 
   `,
   answer:"b"
 }, {
-  question: `Which game does Jowel play most? 
-  a : Pubg
-  b : Mobile Legend
-  c : Far cry
+  question: `What is the team composition in this game? 
+  a : 2v2
+  b : 5v5
+  c : 4v4
 
   `,
   answer:"b"
 },
 {
-  question: `Which brand phone does Jowel have?
-  a : Samsung
-  b : Oppo
-  c : Moto
+  question: `Which is the first hero do you get?
+  a : Alucard
+  b : Eudora
+  c : Layla
 
   `,
   answer:"c"
@@ -68,19 +67,19 @@ console.log('Your total score is '+ score)
 var msg = '';
 switch(score){
   case 0 :
-    msg = chalk.redBright.bold("You don't know Jowel!")
+    msg = chalk.redBright.bold("You don't know about Mobile Legend!")
     break;
   case 1 :
-    msg = chalk.yellowBright.bold("You barely know Jowel!")
+    msg = chalk.yellowBright.bold("You rarely play Mobile Legend!")
     break;
   case 2 :
-    msg = chalk.yellowBright.bold("You barely know Jowel!")
+    msg = chalk.yellowBright.bold("You rarely play Mobile Legend!")
     break;
   case 3:
-    msg = chalk.greenBright.bold("You are good friend with Jowel!")
+    msg = chalk.greenBright.bold("You play Mobile Legend often!")
     break;
   case 4:
-    msg = chalk.greenBright.bold("You know Jowel like no one does!")
+    msg = chalk.greenBright.bold("You are a true Mobile Legend Player!")
     break;
 }
 
